9d566ac3607c322907e33aa3218fec9c0d14474c
Alexey S <sheegalovalexs@gmail.com>
2026-09-01 21:38:25 +0300

    Initial commit

---
+++ BalatroRU.lua
@@ -0,0 +1,14 @@
+SMODS.Language {
+    key = 'ru',
+    label = 'Русский (альт.)',
+    font = {
+        file = 'Handjet-Bold.ttf',
+        render_scale = G.TILESIZE * 10,
+        TEXT_HEIGHT_SCALE = 0.65,
+        TEXT_OFFSET = { x = 0, y = -40 },
+        FONTSCALE = 0.12,
+        squish = 1,
+        DESCSCALE = 1,
+    },
+    loc_key = 'en',
+}
